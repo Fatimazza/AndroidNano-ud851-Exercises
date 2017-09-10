0963a849398e4e795b1115fe6d3e7baeb62e3b49
Fatima Azzahro <fatima@paypro.id>
2017-09-10 22:14:24 +0700

    [Exercise4-1] Declare and initialize TextView in ChildActivity

--- Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -2,18 +2,21 @@ package com.example.android.explicitintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 // COMPLETED (1) Use Android Studio's Activity wizard to create a new Activity called ChildActivity
 
 public class ChildActivity extends AppCompatActivity {
 
-    // TODO (6) Create a TextView field to display your message
+    // COMPLETED (6) Create a TextView field to display your message
+    TextView mDisplayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
 
-        // TODO (7) Get a reference to your TextView in Java
+        // COMPLETED (7) Get a reference to your TextView in Java
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
     }
 }
