884cf7f18acc714a32805961fab43b82084b76c2
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-01-22 14:24:00 +0700

    [Exercise 6-2] Make A PreferenceFragment - Create class Setting Fragment which extends Preference Fragment Compat

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,16 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by fatimazza on 1/22/18.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        
+    }
+}
