6521becb6f6af34a80e573021586bd2ca20e0ea5
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-02-27 20:20:35 +0700

    [Exercise 7-4] Update Adapter SQLite - Replace cursor account Count to entire Cursor

--- Lesson07-Waitlist/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -14,18 +14,20 @@ import com.example.android.waitlist.data.WaitlistContract;
 public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
 
     private Context mContext;
-    // TODO (1) Replace the mCount with a Cursor field called mCursor
-    private int mCount;
+    // COMPLETED (1) Replace the mCount with a Cursor field called mCursor
+    // Holds on to the cursor to display the waitlist
+    private Cursor mCursor;
 
     /**
      * Constructor using the context and the db cursor
      * @param context the calling context/activity
+     * @param cursor the db cursor with waitlist data to display
      */
-    // TODO (2) Modify the constructor to accept a cursor rather than an integer
-    public GuestListAdapter(Context context, int count) {
+    // COMPLETED (2) Modify the constructor to accept a cursor rather than an integer
+    public GuestListAdapter(Context context, Cursor cursor) {
         this.mContext = context;
-        // TODO (3) Set the local mCursor to be equal to cursor
-        mCount = count;
+        // COMPLETED (3) Set the local mCursor to be equal to cursor
+        this.mCursor = cursor;
     }
 
     @Override
@@ -51,8 +53,8 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     @Override
     public int getItemCount() {
-        // TODO (4) Update the getItemCount to return the getCount of mCursor
-        return mCount;
+        // COMPLETED (4) Update the getItemCount to return the getCount of mCursor
+        return mCursor.getCount();
     }
 
 
