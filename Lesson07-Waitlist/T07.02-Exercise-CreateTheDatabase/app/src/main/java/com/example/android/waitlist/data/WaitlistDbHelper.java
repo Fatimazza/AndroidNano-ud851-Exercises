eb0e363922d6cf04cfd17f6b4ffa3c1bd376d1ab
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-02-27 12:23:40 +0700

    [Exercise 7-2] Create Database SQLite - Create Constructor calling its parent with Database Name and Version

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -8,14 +8,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class WaitlistDbHelper extends SQLiteOpenHelper {
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    // COMPLETED (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
 
-    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+    // COMPLETED (3) Create a static final int called DATABASE_VERSION and set it to 1
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    private static final String DATABASE_NAME = "waitlist.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // COMPLETED (4) Create a Constructor that takes a context and calls the parent constructor
 
     public WaitlistDbHelper(Context context) {
-        super(context, "database_name", null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     // TODO (5) Override the onCreate method
