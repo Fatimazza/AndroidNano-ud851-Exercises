733da74ffba0c688248193d68919681c6dfcb62c
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-02-27 11:34:14 +0700

    [Exercise 7-1] Create Contract SQLite - Add inner Class that will contain Table and Columns names

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -4,14 +4,16 @@ import android.provider.BaseColumns;
 
 public class WaitlistContract {
 
-
-
-    // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
+    // COMPLETED (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
+    
+    private static final class WaitListEntry implements BaseColumns {
 
         // TODO (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
+
+    }
 
 }
