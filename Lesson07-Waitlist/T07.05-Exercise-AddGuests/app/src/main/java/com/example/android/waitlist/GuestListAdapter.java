824092986b0a4e935bcfea505484e22af6884a0d
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-02-28 12:06:06 +0700

    [Exercise 7-5] Add Data Guest SQLite - In swapCursor function, Close old Cursor, Update to new Cursor, Refresh the RecyclerView UI

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -66,11 +66,18 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
      */
     private void swapCursor(Cursor newCursor) {
 
-        // TODO (16) Inside, check if the current cursor is not null, and close it if so
+        // COMPLETED (16) Inside, check if the current cursor is not null, and close it if so
+        // Always close the previous mCursor first
+        if (mCursor != null)
+            mCursor.close();
 
-        // TODO (17) Update the local mCursor to be equal to  newCursor
+        // COMPLETED (17) Update the local mCursor to be equal to  newCursor
+        mCursor = newCursor;
 
-        // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        // COMPLETED (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        if (newCursor != null)
+            // Force the RecyclerView to refresh
+            this.notifyDataSetChanged();
     }
 
     /**
