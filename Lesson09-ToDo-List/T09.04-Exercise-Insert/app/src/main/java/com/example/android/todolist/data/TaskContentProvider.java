b543d869aad6d66c0a50e79e5e7948c49a046ba5
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-03-03 15:15:04 +0700

    [Exercise 9-4] Insert in Content Provider - Get access to Database

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -21,6 +21,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
@@ -78,12 +79,12 @@ public class TaskContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
-        // TODO (1) Get access to the task database (to write new data to)
-
         // TODO (2) Write URI matching code to identify the match for the tasks directory
 
         // TODO (3) Insert new values into the database
         // TODO (4) Set the value for the returnedUri and write the default case for unknown URI's
+        // COMPLETED (1) Get access to the task database (to write new data to)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
         // TODO (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
 
